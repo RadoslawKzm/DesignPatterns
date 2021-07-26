9ffea724721de9c6d246b87378bd3c7a798c7527
rdkkz <Bolszewo11>
2021-07-26 22:00:11 +0200

    Strategy pattern added

---
+++ strategy_pattern/ticket_strategy.py
@@ -0,0 +1,75 @@
+"""Ticket ordering example of strategy pattern"""
+from abc import ABC, abstractmethod
+import random
+import string
+from typing import List
+
+
+def generate_id(length=8):
+    # helper function for generating an id
+    return ''.join(random.choices(string.ascii_uppercase, k=length))
+
+
+class SupportTicket:
+
+    def __init__(self, *, customer, issue):
+        self.id: str = generate_id()
+        self.customer: str = customer
+        self.issue: str = issue
+
+
+class TicketStrategy(ABC):
+    """Abstract interface for future strategies of ticket ordering"""
+
+    @abstractmethod
+    def get_ticket(self, tickets: List[SupportTicket]) -> List[SupportTicket]:
+        pass
+
+
+class ReverseTicketStrategy(TicketStrategy):
+    def get_ticket(self, tickets: List[SupportTicket]) -> List[SupportTicket]:
+        list_ = tickets.copy()
+        list_.reverse()
+        return list_
+
+
+class RandomTicketStrategy(TicketStrategy):
+    def get_ticket(self, tickets: List[SupportTicket]) -> List[SupportTicket]:
+        list_ = tickets.copy()
+        random.shuffle(list_)
+        return list_
+
+
+class FIFOTicketStrategy(TicketStrategy):
+    def get_ticket(self, tickets: List[SupportTicket]) -> List[SupportTicket]:
+        return tickets.copy()
+
+
+class CustomerSupport:
+    def __init__(self):
+        self.tickets: List[SupportTicket] = []
+
+    def create_ticket(self, *, customer: str, issue: str):
+        self.tickets.append(SupportTicket(customer=customer, issue=issue))
+
+    def process_tickets(self, *, processing_strategy: TicketStrategy):
+        if not self.tickets:
+            raise ValueError("nothing to process")
+        processed_tickets = processing_strategy.get_ticket(tickets=self.tickets)
+        for ticket in processed_tickets:
+            self.process_ticket(ticket=ticket)
+
+    def process_ticket(self, *, ticket: SupportTicket):
+        print("==================================")
+        print(f"Processing ticket id: {ticket.id}")
+        print(f"Customer: {ticket.customer}")
+        print(f"Issue: {ticket.issue}")
+        print("==================================")
+
+
+app = CustomerSupport()
+app.create_ticket(customer="PGS SOFTWARE", issue="Cannot name all solid principles and all Design patterns")
+app.create_ticket(customer="PGS SOFTWARE", issue="Cannot name all SQL join types")
+app.create_ticket(customer="PGS SOFTWARE", issue="Cannot differentiate Put from POST request, sad")
+
+app.process_tickets(processing_strategy=ReverseTicketStrategy())
